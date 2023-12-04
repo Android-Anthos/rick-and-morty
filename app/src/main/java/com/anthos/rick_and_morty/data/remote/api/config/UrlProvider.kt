9a0e131ec8994f6407f509031238024a3a3389ea
Ruben Z <ruben.zevallos.saavedra@gmail.com>
2023-12-04 11:17:29 -0300

    Creation object class UrlProvider, connection with dependencies (create folder di and object class DataModule)  Retrofit dependency injection is added.Running app without problems.(2 non-use warnings)

--- app/src/main/java/com/anthos/rick_and_morty/data/remote/api/config/UrlProvider.kt
+++ app/src/main/java/com/anthos/rick_and_morty/data/remote/api/config/UrlProvider.kt
@@ -3,5 +3,4 @@ package com.anthos.rick_and_morty.data.remote.api.config
 object UrlProvider {
     private const val BASE_URL = "https://rickandmortyapi.com/api/"
     fun getRickAndMortyUrl() = BASE_URL
-
 }
