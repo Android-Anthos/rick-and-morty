7d687afd1ffbe6671c1587d470cf3c67748c2c35
Astrid Inostroza <flga.astridinostroza@gmail.com>
2023-12-04 00:09:59 -0300

    Se agrega parámetro info en el modelo CharactersApiModel.kt. Se agrega parámetro para permitir paginación

--- app/src/main/java/com/anthos/rick_and_morty/data/remote/model/CharactersApiModel.kt
+++ app/src/main/java/com/anthos/rick_and_morty/data/remote/model/CharactersApiModel.kt
@@ -1,8 +1,10 @@
 package com.anthos.rick_and_morty.data.remote.model
 
+import com.anthos.rick_and_morty.data.remote.model.Constants.INFO
 import com.anthos.rick_and_morty.data.remote.model.Constants.RESULTS
 import com.google.gson.annotations.SerializedName
 
 data class CharactersApiModel(
-    @SerializedName(RESULTS) val results: List<CharacterApiModel>
+    @SerializedName(RESULTS) val results: List<CharacterApiModel?>?,
+    @SerializedName(INFO) val info: InfoApiModel?
 )
